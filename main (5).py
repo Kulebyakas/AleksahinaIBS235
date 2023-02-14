6a0770b7ff2d1346ab2203ea65856f04bf0c5747
Алика Алексахина <124045757+Kulebyakas@users.noreply.github.com>
2023-02-14 14:12:23 +0300

    Пр 1 14.02.23

---
+++ main (5).py
@@ -0,0 +1,9 @@
+tseloye = 10
+drobnoe = 8.4
+stroka = "No"
+big_netseloe = tseloye * 3.5
+drobnoe -=1
+tseloye  / drobnoe
+big_netseloe / drobnoe
+stroka = stroka * 2 + "_" + "YES"
+print (tseloye, drobnoe, stroka, big_netseloe)
